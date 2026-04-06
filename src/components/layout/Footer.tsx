004677b27031af909cf65f19311f6742cda103e5
Zanie567 <alexanderescala@gmail.comgit config --global user.name Zanie567git config --global user.email alexanderescala@gmail.com>
2026-04-06 01:33:46 +0100

    Add Julia Stepniak to founded by list

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -159,7 +159,7 @@ export function Footer() {
               Economics Society. All rights reserved.
             </p>
             <p className="text-cream/20 text-[10px] mt-1">
-              Founded by Alex Escala, Lucas Dwyer and Satvik Singla
+              Founded by Alex Escala, Lucas Dwyer, Satvik Singla and Julia Stepniak
             </p>
           </div>
           <p className="text-cream/20 text-xs">Edinburgh, Scotland</p>
